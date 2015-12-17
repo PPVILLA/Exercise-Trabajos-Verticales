e696b8826e8c54d59a5ed6e5b27a31a9647cac7d
PPVilla <ppvillan@gmail.com>
2015-12-17 14:23:14 +0100

    Añadiendo ficheros CSS y JS de html5-boilerplate y añadiendo en plantillas header y footer los enlaces a los mismos

--- application/view/_templates/footer.php
+++ application/view/_templates/footer.php
@@ -19,5 +19,8 @@
 
     <!-- our JavaScript -->
     <script src="<?php echo Config::get('URL'); ?>js/application.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.3.min.js"><\/script>')</script>
+    <script src="js/plugins.js"></script>
+
 </body>
 </html>
--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -1,13 +1,24 @@
 <!doctype html>
-<html>
+<html class="no-js" lang="es">
 <head>
-    <title>Trabajos Verticales</title>
-    <!-- META -->
     <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Trabajos Verticales Reparacion fachada</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <!-- Place favicon.ico in the root directory -->
+
     <!-- send empty favicon fallback to prevent user's browser hitting the server for lots of favicon requests resulting in 404s -->
     <link rel="icon" href="data:;base64,=">
-    <!-- CSS -->
+
+    <!-- PHPHUGE CSS -->
     <!-- <link rel="stylesheet" href="<?php echo Config::get('URL'); ?>css/style.css" /> -->
+    <!-- html5-boilerplate CSS
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css"> -->
+
     <!-- Bootstrap core CSS -->
     <link href="<?php echo Config::get('URL');?>css/bootstrap.css" rel="stylesheet">
     <!-- Custom styles for this template
@@ -16,14 +27,14 @@
     <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
     <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
     <script src="<?php echo Config::get('URL'); ?>js/ie-emulation-modes-warning.js"></script>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+    <script src="js/vendor/modernizr-2.8.3.min.js"></script>
 </head>
 <body>
+    <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
     <!-- wrapper, to center website -->
     <!-- <div class="wrapper"> -->
 
