b45ff291c17dacb4c09d23132327cdcddc2020f4
PPVilla <ppvillan@gmail.com>
2016-01-13 11:40:32 +0100

    corrigiendo pagina index

--- application/view/index/index.php
+++ application/view/index/index.php
@@ -1,60 +1,60 @@
-    <main class= "container no-padding">
-      <img class="responsive-img bg" src="img/portada.jpg" alt="Portada ADV Trabajos Verticales Cadiz">
-      <div class="row">
-        <div class="col s3">
-            <img class="responsive-img" src="img/adv-trabajos-verticales-cadiz.jpg" alt="ADV Trabajos Verticales Cadiz">
-        </div>
-      <div class="row">
-        <div class="col s12">
-          <h1><strong>¡¡Lo que buscabas!!</strong></h1>
-          <h3 class="flow-text"><strong>Trabajos en alturas en lugares de díficil acceso mediante sistemas de acceso y posicionamiento con técnicas de doble cuerda.</strong></h3>
-          <h3 class="flow-text"><strong>Realizar cualquier tipo de trabajo en menos tiempo y con la misma efectividad que utilizando medios convencionales como andamios, plataformas o gruas, teniendo en cuenta en en algunos lugares no se pueden utilizar por motivos de espacio, tráfico, accesibilidad, economía, seguridad, robos, labores puntuales, etc.</strong></h3>
-          <p><a class="btn waves-effect waves-light" href="#">Saber más &raquo;</a></p>
-        </div>
-      </div>
+    <main class= "card">
+      <section class="card-image">
+        <img src="img/portada.jpg" alt="Portada ADV Trabajos Verticales Cadiz">
+        <article class="card-title">
+          <div class="row">
+            <div class="col s3">
+                <img src="img/adv-trabajos-verticales-cadiz.jpg" alt="ADV Trabajos Verticales Cadiz">
+            </div>
+          </div>
+          <div class="row">
+            <div class="col s9">
+              <h3 class="flow-text">¡¡Lo que buscabas!!</h3>
+              <h4 class="flow-text">Trabajos en alturas en lugares de díficil acceso mediante sistemas de acceso y posicionamiento con técnicas de doble cuerda. Realizar cualquier tipo de trabajo en menos tiempo y con la misma efectividad que utilizando medios convencionales como andamios, plataformas o gruas, teniendo en cuenta en en algunos lugares no se pueden utilizar por motivos de espacio, tráfico, accesibilidad, economía, seguridad, robos, labores puntuales, etc.</h4>
+            </div>
+          </div>
+        </article>
+      </section>
     </main>
 
-    <section class="container">
+    <section class="row">
       <!-- Example row of columns -->
-      <div class="row">
-        <article class="col s12 m4">
-        <h2>Quienes somos</h2>
-          <p>Somos ADV Trabajos Verticales, una empresa con dilatada experiencia en  el sector de trabajos en altura en la provincia de Cádiz. Nuestros servicios están dirigidos a toda clase de actividad en lugares de difícil acceso mediante técnicas de escalada aplicadas a la edificación e industria, sin utilizar andamios ni grúas. Accedemos al lugar en un corto espacio de tiempo, realizamos nuestra labor y recogemos en el mismo día sin ningún tipo de problemas e inconvenientes para el usuario.  Consulte con nosotros. Usamos todas las primeras marcas del mercado,no rebajamos costos en materiales. El ahorro económico está en la utilización de técnicas rápidas y seguras. </p>
-          <p><a class="btn waves-effect waves-light" href="#">Ver detalles &raquo;</a></p>
-        </article>
-        <article class="col s12 m4">
-          <h2>Dónde estamos</h2>
-          <address>
-          <strong>ADV Trabajos Verticales</strong><br>
-          Calle Sol, 54 <br>
-          11560 Trebujena (Cádiz)<br>
-          <a href="mailto:#">E-mail: info@advtrabajosverticales.com</a><br>
-          <abbr title="Telefono">Tlfno:</abbr> 652 181 935
-          </address>
-          <p><a class="btn waves-effect waves-light" href="#" >Ver detalles &raquo;</a></p>
-       </article>
-        <article class="col s12 m4">
-          <h2>Qué ofrecemos</h2>
-          <li>Aislamientos sin obras.</li>
-          <li>Rehabilitación energética fácil.</li>
-          <li>Sellado de grietas juntas de dilatación.</li>
-          <li>Reparación de desperfectos en fachadas y patios.</li>
-          <li>Limpieza paredes,ladrillos,piedras,zócalos.</li>
-          <li>Pintura y  tratamientos hidrófugos de parámentos verticales.</li>
-          <li>Impermeabilización de cubiertas.</li>
-          <li>Dispositivos antiaves en edificios.</li>
-          <li>Montajes carteles y rótulos publicitarios.</li>
-          <li>Instalaciones de canalizaciones verticales.</li>
-          <li>Limpieza de cristales, persianas, muros cortina.</li>
-          <li>Montaje en torres de telecomunicaciones y eléctricas.</li>
-          <li>Trabajos en aerogeneradores.</li>
-          <p><a class="btn waves-effect waves-light" href="#" >Ver detalles &raquo;</a></p>
-        </article>
-      </div>
-
+      <article class="col s12 m4">
+      <h2>Quienes somos</h2>
+        <p>Somos ADV Trabajos Verticales, una empresa con dilatada experiencia en  el sector de trabajos en altura en la provincia de Cádiz. Nuestros servicios están dirigidos a toda clase de actividad en lugares de difícil acceso mediante técnicas de escalada aplicadas a la edificación e industria, sin utilizar andamios ni grúas. Accedemos al lugar en un corto espacio de tiempo, realizamos nuestra labor y recogemos en el mismo día sin ningún tipo de problemas e inconvenientes para el usuario.  Consulte con nosotros. Usamos todas las primeras marcas del mercado,no rebajamos costos en materiales. El ahorro económico está en la utilización de técnicas rápidas y seguras. </p>
+        <p><a class="btn waves-effect waves-light" href="#">Ver detalles &raquo;</a></p>
+      </article>
+      <article class="col s12 m4">
+        <h2>Dónde estamos</h2>
+        <address>
+        <strong>ADV Trabajos Verticales</strong><br>
+        Calle Sol, 54 <br>
+        11560 Trebujena (Cádiz)<br>
+        <a href="mailto:#">E-mail: info@advtrabajosverticales.com</a><br>
+        <abbr title="Telefono">Tlfno:</abbr> 652 181 935
+        </address>
+        <p><a class="btn waves-effect waves-light" href="#" >Ver detalles &raquo;</a></p>
+     </article>
+      <article class="col s12 m4">
+        <h2>Qué ofrecemos</h2>
+        <li>Aislamientos sin obras.</li>
+        <li>Rehabilitación energética fácil.</li>
+        <li>Sellado de grietas juntas de dilatación.</li>
+        <li>Reparación de desperfectos en fachadas y patios.</li>
+        <li>Limpieza paredes,ladrillos,piedras,zócalos.</li>
+        <li>Pintura y  tratamientos hidrófugos de parámentos verticales.</li>
+        <li>Impermeabilización de cubiertas.</li>
+        <li>Dispositivos antiaves en edificios.</li>
+        <li>Montajes carteles y rótulos publicitarios.</li>
+        <li>Instalaciones de canalizaciones verticales.</li>
+        <li>Limpieza de cristales, persianas, muros cortina.</li>
+        <li>Montaje en torres de telecomunicaciones y eléctricas.</li>
+        <li>Trabajos en aerogeneradores.</li>
+        <p><a class="btn waves-effect waves-light" href="#" >Ver detalles &raquo;</a></p>
+      </article>
       <hr>
-
-      <footer>
-        <p>&copy; ADV Trabajos Verticales 2015</p>
-      </footer>
     </section> <!-- /section -->
+
+    <footer>
+      <p>&copy; ADV Trabajos Verticales 2015</p>
+    </footer>
